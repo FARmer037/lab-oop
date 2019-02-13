2629c6488ccac3597d6a2067d24a66b22a750c08
FARmer037 <mustofa0367@gmail.com>
2019-02-13 23:14:36 +0700

    prim's code

--- lab3 - ArrayList/Main.java
+++ lab3 - ArrayList/Main.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
 import java.util.Iterator;
-public class Main {
-    public static void main(String[] args) {
+
+public class Main{
+    public static void main(String args[]) {
         testList();
         System.out.println(" ");
         //Iterator
         StudentList st = new StudentList();
-        st.addStudent(new Student(10, "janejira", 2.58));
-        st.addStudent(new Student(11, "kamonnet", 3.90));
-        st.addStudent(new Student(12, "kratai", 2.82));
+        st.addStudent(new Student(10,"janejira",2.58));
+        st.addStudent(new Student(11,"kamonnet",3.90));
+        st.addStudent(new Student(12,"kratai",2.82));
             System.out.println("addStudent:");  
         st.showAllStudentInfo();
             System.out.println("number of Student:" +st.number());
@@ -19,44 +20,43 @@ public class Main {
             System.out.println("number of Student:" +st.number());
             System.out.println(" ");
             System.out.println("searchStudent[1]:");
-        st.searchStudentBystCode(1);
+            st.searchStudentBystCode(1);
             System.out.println(" ");
     }
-
     public static void testList(){
         ArrayList<Student> students = new ArrayList<Student>();
 
-        Student std1 = new Student();
-        std1.setName("keerati");
-        std1.setCode(111);
-        std1.setGPA(3.20);
-        students.add(std1);
+        Student mhoo = new Student();
+        mhoo.setName("keerati");
+        mhoo.setCode(111);
+        mhoo.setGPA(3.20);
+        students.add(mhoo);
 
-        Student std2 = new Student();
-        std2.setName("janjira");
-        std2.setCode(220);
-        std2.setGPA(2.80);
-        students.add(std2);
+        Student nam = new Student();
+        nam.setName("janjira");
+        nam.setCode(220);
+        nam.setGPA(2.80);
+        students.add(nam);
 
-        Student std3 = new Student();
-        std3.setName("penpan");
-        std3.setCode(121);
-        std3.setGPA( 3.50);
-        students.add(std3);
+        Student kwang = new Student();
+        kwang.setName("penpan");
+        kwang.setCode(121);
+        kwang.setGPA( 3.50);
+        students.add(kwang);
 
         for (Student st: students) {   //forech
-            System.out.println(st);
+            System.out.println(st);  
         }
         System.out.println("number of Student:" +students.size()); 
         System.out.println(" ");
-        students.remove(std2);
+        students.remove(nam);
         /* Student get1 = students.get(1);    
         System.out.println(get1);
         */ 
         Iterator<Student> it = students.iterator();    //Iterator
             while(it.hasNext()) {
-                 Student obj = (Student)it.next();
-                 System.out.println(obj);    
-                } System.out.println("number of Student:" +students.size());      
+                Student obj = (Student)it.next();
+                System.out.println(obj);    
+            } System.out.println("number of Student:" +students.size());      
     }  
 }
--- lab3 - ArrayList/StudentList.java
+++ lab3 - ArrayList/StudentList.java
@@ -1,43 +1,41 @@
 import java.util.ArrayList;
 public class StudentList {
-    private ArrayList<Student> stdList;
+    private ArrayList<Student> stList;
     private int numberOfStudent;
-
-    public StudentList() {
-        stdList = new ArrayList<Student>();
-        numberOfStudent = 0;
+  
+    public StudentList(){  
+        stList = new ArrayList<Student>();  
+        numberOfStudent=0;
     }
-
-    public void addStudent(Student theStudent) {
-        stdList.add(theStudent);
+    public void addStudent(Student students){  
+        stList.add(students); 
     }
-
-    public void removeStudentByCode(int stCode) {
-        if ((stCode >= 0) && (stCode < stdList.size())) {
-            stdList.remove(stCode);
+    public void removeStudentBystCode(int stCode){
+        if ((stCode >= 0) && (stCode < stList.size())) {
+            stList.remove(stCode);
         }
     }
-
     public void showAllStudentInfo() {
-        for (Student student : stdList) {
-            System.out.println(student);
+        for (Student st: stList) {
+            System.out.println(st);
         }
     }
 
-    public int getNumberOfStudent() {
-        return this.numberOfStudent;
+    public int getNumberOfStudent (){  
+        return numberOfStudent;  
+    }
+
+    public int number () {  
+        return stList.size();    
     }  
-    public int number() {
-        return stdList.size();    
-    }
-
     public Student searchStudentBystCode(int stCode) {
-        Student std = null;
-        if ((stCode >= 0)&&(stCode < stdList.size())){ // if a valid note number
-            std = stdList.get(stCode);
-            System.out.println(stdList.get(stCode) );
-        }
-    return std;
-    // System.out.println(stList.get(stCode) );
+        Student st=null;
+        if ((stCode >= 0)&&(stCode < stList.size())) { // if a valid note number
+            st = stList.get(stCode);
+            System.out.println(stList.get(stCode) );
     }
-}
+    return st;
+    // System.out.println(stList.get(stCode) );
+  
+    }
+}  
